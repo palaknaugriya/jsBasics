4552df572973b77ef58ca9e33f044a82675ff178
Palak <palak.gupta@mapmyindia.com>
2021-02-01 00:03:26 +0530

    add middleware

--- Express/routers/user.js
+++ Express/routers/user.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+// middleware function -> function that has access to request object and response object,
+// as well as the next middleware function in the request-response cycle.
+router.use('/getUser', (req, res, next) => {
+    console.log('Inside Middleware')
+    next()
+})
+
 router.get('/getUser', (req, res) => {
+    console.log('Inside Router')
     res.send({
         'name': 'Palak',
         'age': 24
